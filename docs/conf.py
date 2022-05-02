e20244e61548c9c4e368285d85eec468c5ed9f2d
joeledwardson <joel.edwardson1@gmail.com>
2022-05-02 07:50:43 +0100

    added intershpinx

--- docs/conf.py
+++ docs/conf.py
@@ -28,8 +28,11 @@ author = 'Joel Edwardson'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc'
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx'
 ]
+
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
