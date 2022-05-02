e3a1690fda2028277cf1bf0cb1eb1db9d1f0d71a
joeledwardson <joel.edwardson1@gmail.com>
2022-05-02 07:44:40 +0100

    added docstring tests for numeric

--- joelsutilities/numeric.py
+++ joelsutilities/numeric.py
@@ -6,7 +6,23 @@ from .exceptions import NumericException
 
 
 def closest_index(array: np.ndarray, value: Union[float, int]) -> int:
-    """get closest index in numpy array, or -1 if empty"""
+    """
+    get closest index in numpy array, or -1 if empty
+    
+    
+    >>> closest_index([1,2,3,4,5], 1.2)
+    0
+    >>> closest_index([1,2,3,4,5], -1)
+    0
+    >>> closest_index([1,2,3,4,5], 1.6)
+    1
+    >>> closest_index([1,2,3,4,5], 999)
+    4
+    >>> closest_index([1,2,3,4,5], 2.4)
+    1
+    >>> closest_index([1,2,3,4,5], 4.9)
+    4
+    """
 
     if array.ndim != 1:
         raise NumericException(f"expected 1-dimensional array, got: {array.ndim}")
